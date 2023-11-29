7423128ffff047a425fa76497a6f31fe7314d280
Ediana-Ekanem <edianaekanem@gmail.com>
2023-11-29 15:43:27 +0100

    latest commit

--- src/index.js
+++ src/index.js
@@ -7,12 +7,8 @@ function App() {
   return (
     <div className="container">
       <Avatar />
-
-      <div>
-        <Heading />
-
-        <Skillset />
-      </div>
+      <Heading />
+      <Skillset />
     </div>
   );
 }
@@ -26,8 +22,8 @@ function Heading() {
       <h1 className="heading">Ediana Ekanem</h1>
       <p className="heading" style={{ padding: "0 5px" }}>
         Front End Developer and teacher at Start Innovation hub, Uyo, Nigeria. I
-        love music and playing video games, I cook and i love good food as well
-        I speak Ibibio, English and Pigin English
+        love music and playing video games, I cook and i love good food as well.
+        I speak Ibibio, English and Pigin English.
       </p>
     </div>
   );
@@ -36,11 +32,11 @@ function Skillset() {
   return (
     <div className="skillset">
       <Skill skill="Html " emoji="💪" color="#FF0000" />
-      <Skill skill="Css " emoji="💪" color="#722F37" />
+      <Skill skill="Css " emoji="💪" color="#563d7c" />
       <Skill skill="Javascript " emoji="👶" color="#FFFF00" />
-      <Skill skill="Git and GitHub " emoji="💪" color="#0000FF" />
+      <Skill skill="Git and GitHub " emoji="💪" color="#98fb98" />
       <Skill skill="React " emoji="👶" color="#ADD8E6" />
-      <Skill skill="Linux " emoji="👶" color="#FFC0CB" />
+      <Skill skill="Linux " emoji="👶" color="	#C0C0C0" />
     </div>
   );
 }
